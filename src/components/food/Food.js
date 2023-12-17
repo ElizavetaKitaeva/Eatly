84edde90a3658ad1aa2cf250c4aff0d30341161a
Eilla <eilla@student.21-school.ru>
2023-12-17 23:07:44 +0700

    create banner

--- src/components/food/Food.js
+++ src/components/food/Food.js
@@ -1,11 +1,28 @@
 import food from './Food.svg'
+import Graph from './Graph.png'
 import './style.scss'
 
 const Food = () => {
   return (
-    <div className="food">
-      <img className="food__image" src={food} alt="food" />
+    <div className="plate">
+      <div className="plate-food">
+        <img className="food__image" src={food} alt="food" />
+      </div>
+      <div className="card-onTheWay">
+        <div className="card__info">
+          <img className="food__image card__image" src={food} alt="food" />
+          <article>
+            <p className="card__title">Chicken Hell</p>
+            <p className="card__text">On The Way</p>
+          </article>
+        </div>
+        <p className="card__time">3:09 PM</p>
+      </div>
+      <div className="card-stats">
+        <img className="stats__image" src={Graph} alt="Graph" />
+      </div>
     </div>
+
   );
 }
  
